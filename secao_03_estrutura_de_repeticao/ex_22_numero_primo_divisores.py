80cf96e1d8fdcd0ee3702a59fed1bc3493901a09
rodrigo <rodrigosluz@gmail.com>
2022-06-24 13:39:45 -0300

    Inclui novos arquivos e resolução do ex 22 da seção 03

--- secao_03_estrutura_de_repeticao/ex_22_numero_primo_divisores.py
+++ secao_03_estrutura_de_repeticao/ex_22_numero_primo_divisores.py
@@ -50,3 +50,17 @@ divisível.
 
 def eh_primo(n: int) -> bool:
     """Escreva aqui em baixo a sua solução"""
+    x = 0
+    divisores = 0
+    if n >= 2:
+        for i in range(n):
+            i += 1
+            if n % i == 0:
+                x += 1
+                if i != 1 and i != n:
+                    print(f'É divisível por {i}')
+
+    if x == 2:
+        print(True)
+    else:
+        print(False)
